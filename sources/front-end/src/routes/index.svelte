55bb9d455b7295376373c8a74d938f7767740a93
Dmitry N. Medvedev <dmitry.medvedev@gmail.com>
2020-10-06 21:06:47 +0200

    added AuthScreen and OrdersScreen stubs
    Signed-off-by: Dmitry N. Medvedev <dmitry.medvedev@gmail.com>

--- sources/front-end/src/routes/index.svelte
+++ sources/front-end/src/routes/index.svelte
@@ -1,6 +1,19 @@
+<script>
+	import AuthScreen from "../components/AuthScreen/AuthScreen.svelte";
+	import OrdersScreen from '../components/OrdersScreen/OrdersScreen.svelte';
+
+	let isAuthenticated = false;
+</script>
+
 <style>
 </style>
 
 <svelte:head>
 	<title>c3</title>
 </svelte:head>
+
+{#if isAuthenticated === true}
+	<OrdersScreen />
+{:else}
+	<AuthScreen />
+{/if}
